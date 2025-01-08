37bbb5ed394dd5d2904a407509b889dba8707ad6
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-01-09 01:52:13 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// rafce
+import React from 'react'
+import Sidebar from "./components/Sidebar/Sidebar"
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
