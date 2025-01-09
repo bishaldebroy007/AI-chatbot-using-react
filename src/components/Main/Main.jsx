7aa8d381509572178bbd569c196acb625f1fe657
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-01-09 15:17:13 +0600

    search box basic staff done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -32,6 +32,20 @@ const Main = () => {
                         <img src={assets.bulb_icon} alt="" />
                     </div>
                 </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a prompt here...' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.menu_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        Gemini Clone may display inaccurate info, including about people, so doube-check the inforfation.
+                    </p>
+                </div>
+
             </div>
 
         </div>
