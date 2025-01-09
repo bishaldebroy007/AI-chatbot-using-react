dd0c450ed5b28c75add2817a91f4e89c070d7dce
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-01-09 12:38:32 +0600

    state done in sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <img className="menu" src={assets.menu_icon} alt="" />
+        <img onClick={() => setExtended(prev => !prev)} className="menu" src={assets.menu_icon} alt="" />
         <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
@@ -29,22 +29,25 @@ const Sidebar = () => {
       <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
-          <p>Help</p>
+          {extended ? <p>Help</p> : null}
         </div>
       </div>
 
-      <div className="bottom">
-        <div className="bottom-item recent-entry">
-          <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
-        </div>
+
+      <div className="bottom-item recent-entry">
+        <img src={assets.history_icon} alt="" />
+        {extended ? <p>Activity</p> : null}
       </div>
 
-      <div className="bottom">
-        <div className="bottom-item recent-entry">
-          <img src={assets.setting_icon} alt="" />
-          <p>Settings</p>
-        </div>
+
+
+      <div className="bottom-item recent-entry">
+        <img src={assets.setting_icon} alt="" />
+        {extended ? <p>Settings</p> : null}
+
+
+        {/* https://youtu.be/0yboGn8errU?si=jsV18i8fP-L1j6sj&t=1530 */}
+
       </div>
     </div>
   );
